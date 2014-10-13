f0eb499da1361fb650fe4ae14f930b60f7e465ec
Pruebauser <Prueba@mail.com>
2014-10-13 20:13:19 +0200

    aqui, probando esta ñocla

---
+++ src/Practica1_10.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import javax.swing.JOptionPane;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
+
+
+public class Practica1_10 {
+
+	public static void main(String [] args) throws IOException {
+	
+		String elige = JOptionPane.showInputDialog("Cómo quieres cargar el contenido?"
+				+"\n1) Árbol DOM"
+				+"\n2) SAX");
+	
+
+
+	}
+}
